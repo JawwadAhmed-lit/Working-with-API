042c7cdcbdca0f2abefce32b40f94a1c9a0c62ca
JawwadAhmed-lit <jawwadahmed747@gmail.com>
2020-08-14 17:50:36 +0400

    added some new colours

--- python_repos _visual.py
+++ python_repos _visual.py
@@ -8,25 +8,48 @@ url ='https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers ={'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Status Code:{r.status_code}")
+
 # Store API response in a variable.
 # process the results
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars =[], []
+
+repo_names, stars, labels =[], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owners = repo_dict['owner']['login']  
+    description = repo_dict['description']
+    label =f"{owners}<br />{description}"
+    labels.append(label)
+
 # Make visualization
 data = [{
     'type' : 'bar',
     'x': repo_names,
     'y': stars,
+    'hovertext': labels,
+    'marker':{
+        'color':'rgb(60, 100 ,150)',
+        'line':{'width':1.5,'color':'rgb(25,25,25)'}
+    },
+    'opacity': 0.6,
 }]
 
 my_layout={
     'title':'Most-Starred Python Projects on GitHub',
-    'xaxis':{'title':'Repository'},
-    'yaxis':{'title':'Stars'},
+    'titlefont':{'size':28},
+    'xaxis':{
+        'title':'Repository',
+        'titlefont':{'size':24},
+        'tickfont':{'size':14},
+        },
+    'yaxis':{
+        'title':'Stars',
+        'titlefont':{'size':24},
+        'tickfont':{'size':14},
+        },
 }
 fig ={'data':data, 'layout':my_layout}
-offline.plot(fig, filename="python_repos.html")
+offline.plot(fig, filename="python_repos_visual.html")
